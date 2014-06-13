a79ab64194a06ea6e55c16c849634350fa532f80
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-06-13 13:47:41 +0800

    Add comments to Categories

--- pixnet-sdk/src/main/java/net/pixnet/sdk/response/CategoriesList.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/CategoriesList.java
@@ -3,5 +3,8 @@ package net.pixnet.sdk.response;
 import java.util.ArrayList;
 
 public class CategoriesList{
+    /**
+     * List of categories
+     */
     public ArrayList<Category> Categories;
 }
--- pixnet-sdk/src/main/java/net/pixnet/sdk/response/Category.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/Category.java
@@ -3,13 +3,40 @@ package net.pixnet.sdk.response;
 import java.util.ArrayList;
 
 public class Category{
+    /**
+     * Category id
+     */
     public String id;
+    /**
+     * Category type
+     */
     public String type;
+    /**
+     * Order
+     */
     public String order;
+    /**
+     * Category name
+     */
     public String name;
+    /**
+     * Category description
+     */
     public String description;
+    /**
+     * Category index
+     */
     public String show_index;
+    /**
+     * site_category_id
+     */
     public String site_category_id;
+    /**
+     * site_category_done
+     */
     public String site_category_done;
+    /**
+     * Child categories list
+     */
     public ArrayList<Category> child_categories;
 }
