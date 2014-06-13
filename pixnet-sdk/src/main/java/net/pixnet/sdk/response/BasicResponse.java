35ac4e84b367052b1bc5a7f06457913759847aa9
ben <ben@pixnet.tw>
2014-06-13 10:02:51 +0800

    add comment for javadoc

--- pixnet-sdk/src/main/java/net/pixnet/sdk/response/BasicResponse.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/BasicResponse.java
@@ -1,9 +1,15 @@
 package net.pixnet.sdk.response;
 
 /**
- * Created by pengshio on 2014/6/13.
+ * Created by Ben on 2014/6/13.
  */
 public class BasicResponse {
+    /**
+     * response message
+     */
     public String message;
+    /**
+     * error code
+     */
     public String error;
 }
