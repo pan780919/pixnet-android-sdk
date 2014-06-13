4c9395a2ad8590cba7c22597e44d59463fdf56ce
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-06-13 17:48:48 +0800

    Add Subscription_groupList.java Subscription_group.java

---
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/Subscription_group.java
@@ -0,0 +1,7 @@
+package net.pixnet.sdk.response;
+
+public class Subscription_group{
+    public String id;
+    public String name;
+    public String position;
+}
---
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/Subscription_groupList.java
@@ -0,0 +1,8 @@
+package net.pixnet.sdk.response;
+
+import java.util.ArrayList;
+
+public class Subscription_groupList extends BasicResponse{
+    public ArrayList<Subscription_group> subscription_groups;
+    public int total;
+}
