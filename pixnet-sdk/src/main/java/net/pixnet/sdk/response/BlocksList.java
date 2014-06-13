a4e809cd9ef06182d00cf842c62f2bdb21ab18cd
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-06-13 17:08:24 +0800

    Add Block.java BlocksList.java

---
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/BlocksList.java
@@ -0,0 +1,13 @@
+package net.pixnet.sdk.response;
+
+import java.util.ArrayList;
+
+/**
+ * Blocks list
+ */
+public class BlocksList{
+    /**
+     * Blocks
+     */
+    public ArrayList<Block> blocks;
+}
