9326eea9f692e4fef3e97a69831f9d64f3753854
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-06-13 15:28:11 +0800

    Add COnfig.java

---
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/Config.java
@@ -0,0 +1,10 @@
+package net.pixnet.sdk.response;
+
+public class Config extends BasicResponse{
+    public int is_vip;
+    public int is_rotatebyexif;
+    public String is_elementfirst;
+    public String user_avatar;
+    public String user_quota;
+    public String user_used_quota;
+}
