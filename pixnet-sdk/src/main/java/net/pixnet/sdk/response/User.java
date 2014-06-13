5447fee46a4193d5b20e36ea52f1b5f7cb910201
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-06-13 11:25:24 +0800

    Add User.java

---
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/User.java
@@ -0,0 +1,11 @@
+package net.pixnet.sdk.response;
+
+public class User{
+    public String name;
+    public String display_name;
+    public String avatar;
+    public String cavatar;
+    public String link;
+    public boolean is_vip;
+    public boolean has_ad;
+}
