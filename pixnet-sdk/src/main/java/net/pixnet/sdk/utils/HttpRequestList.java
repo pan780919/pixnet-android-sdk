dd31a0710e23d55e587ce7699f8a3087ade2bbaa
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-06-27 16:38:52 +0800

    ChangeHashmap to SparseArray for better performance

--- pixnet-sdk/src/main/java/net/pixnet/sdk/utils/HttpRequestList.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/utils/HttpRequestList.java
@@ -1,12 +1,14 @@
 package net.pixnet.sdk.utils;
 
+import android.util.SparseArray;
+
 import java.util.HashMap;
 
 /**
- * A hash_map stored HttpRequests and control amount of requests
+ * A SparseArray stored HttpRequests and control amount of requests
  */
 public class HttpRequestList {
-    private HashMap<Integer, HttpHelper> map = new HashMap<Integer, HttpHelper>();
+    private SparseArray<HttpHelper> map = new SparseArray<HttpHelper>();
     private int count = 0;
     private int limit = 3;
 
@@ -18,12 +20,12 @@ public class HttpRequestList {
         this.limit = limit;
     }
     /**
-     * If reach the limit then stuck the request , else put the request into the hash_map.
+     * If reach the limit then stuck the request , else put the request into the SparseArray.
      * @param request Http_request
      * @return the position of the request
      */
     public int addRequest(HttpHelper request) {
-        if (count < limit) {
+        if (map.size() < limit) {
             map.put(count, request);
             count++;
             return count - 1;
@@ -32,23 +34,23 @@ public class HttpRequestList {
     }
 
     /**
-     * remove the request at position - key from the hash_map.
+     * remove the request at position - key from the SparseArray.
      * @param key the position where you want to remove a request.
      */
     public void removeRequest(int key) {
-        map.remove(key);
-        count--;
+        map.delete(key);
     }
 
     /**
-     * cancel the request at position - key from the hash_map.
+     * cancel the request at position - key from the SparseArray.
      * @param key the position where you want to cancel a request.
      */
     public void cancelRequest(int key) {
         try {
-            HttpHelper hh = map.get(key);
+            HttpHelper hh = map.get(key,null);
+            if(hh!=null)
             hh.cancel();
-        } catch (IndexOutOfBoundsException e) {
+        } catch (Exception e) {
 
         }
     }
