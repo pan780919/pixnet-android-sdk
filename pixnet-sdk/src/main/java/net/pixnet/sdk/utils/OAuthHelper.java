49756734365315a87954e5474175d92ac56f07c0
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-06-27 17:08:11 +0800

    Add get ver2 in OAuthHelper.java

--- pixnet-sdk/src/main/java/net/pixnet/sdk/utils/OAuthHelper.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/utils/OAuthHelper.java
@@ -16,7 +16,6 @@ import java.security.Key;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
 
 import javax.crypto.Mac;
