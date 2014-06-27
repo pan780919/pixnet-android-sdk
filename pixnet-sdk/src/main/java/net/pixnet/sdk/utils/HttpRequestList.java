10b2508464c9b119f57da679015a5894151c96a8
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-06-27 15:53:26 +0800

    Remove some unuse imports in HttpRequestList.java

--- pixnet-sdk/src/main/java/net/pixnet/sdk/utils/HttpRequestList.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/utils/HttpRequestList.java
@@ -1,33 +1,31 @@
 package net.pixnet.sdk.utils;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
-/**
- * Created by Koi on 2014/6/27.
- */
+
 public class HttpRequestList {
-    HashMap<Integer,HttpHelper> map = new HashMap<Integer,HttpHelper>();
+    HashMap<Integer, HttpHelper> map = new HashMap<Integer, HttpHelper>();
     int count = 0;
 
-    public int addRequest(HttpHelper request){
-        if(count<3){
-            map.put(count,request);
+    public int addRequest(HttpHelper request) {
+        if (count < 3) {
+            map.put(count, request);
             count++;
-            return count-1;
-        }else
-        return -1;
+            return count - 1;
+        } else
+            return -1;
     }
-    public void removeRequest(int key){
+
+    public void removeRequest(int key) {
         map.remove(key);
         count--;
     }
-    public void cancelRequest(int key){
+
+    public void cancelRequest(int key) {
         try {
             HttpHelper hh = map.get(key);
             hh.cancel();
-        }catch(IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
 
         }
     }
