7dbbe700e4d0a39389116b054baefd9b77fced4a
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-07-04 17:36:04 +0800

    Thread go die , instead of AsyncTask

--- pixnet-sdk/src/main/java/net/pixnet/sdk/utils/RequestController.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/utils/RequestController.java
@@ -1,5 +1,6 @@
 package net.pixnet.sdk.utils;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -15,55 +16,94 @@ import java.util.Queue;
  */
 public class RequestController {
     private Queue<Request> requestQueue = new LinkedList<Request>();
-    private Thread worker;
     private OAuthHelper reqOauth;
+    private int taskLimit = 3;
+    private int taskSize = 0;
 
     /**
-     *
      * @param client_id
      * @param client_secret
      */
     RequestController(String client_id, String client_secret) {
         reqOauth = new OAuthHelper(OAuthHelper.OAuthVersion.VER_2, client_id, client_secret);
-        runWorker();
     }
 
     /**
-     * Create a thread to check if their are request in the queue
+     * Set Tasks limits
      */
-    private void runWorker(){
-        worker = new Thread(new Runnable() {
-            public void run() {
-                Request request;
-                while ((request = requestQueue.poll()) != null) {
-                    String httpMethod = request.getHttpMethod();
-                    String response = "";
-                    if (httpMethod == "GET") {
-                        response = reqOauth.get(request.getRequestURL(), request.getParams());
-                    } else if (httpMethod == "POST") {
-                        response = reqOauth.post(request.getRequestURL(), request.getParams());
-                    } else if (httpMethod == "DELECT") {
-                        response = reqOauth.delete(request.getRequestURL(), request.getParams());
-                    }
-                    if (request.getHandler() != null) {
-                        Handler handler = request.getHandler();
-                        Message msg = new Message();
-                        Bundle bundle = new Bundle();
-                        bundle.putString("res", response);
-                        msg.setData(bundle);
-                        handler.sendMessage(msg);
-                    } else {
-                        request.getCallback().callback(response);
-                    }
-                }
-
+    public void setLimit(int taskLimit){
+        this.taskLimit = taskLimit;
+    }
+    /**
+     * Create a AsyncTask to check if their are request in the queue
+     */
+    private void controller() {
+        Request request;
+        while (taskSize < taskLimit) {
+            if ((request = requestQueue.poll()) != null) {
+                taskSize++;
+                runWorker(request);
+            } else {
+                break;
             }
-        });
-        worker.start();
+        }
+    }
+
+    /**
+     * Controller to handle responses
+     *
+     * @param request
+     * @param response
+     */
+    private void controller(Request request, String response) {
+        if (request.getHandler() != null) {
+            Handler handler = request.getHandler();
+            Message msg = new Message();
+            Bundle bundle = new Bundle();
+            bundle.putString("res", response);
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+        } else {
+            request.getCallback().callback(response);
+        }
+        taskSize--;
+        controller();
+    }
+
+    /**
+     * Worker AsyncTask instead of Thread
+     */
+    private class Worker extends AsyncTask<Request, Void, String> {
+        Request request;
+
+        protected String doInBackground(Request... requestArray) {
+            request = requestArray[0];
+            String response = "";
+            if (request != null) {
+                String httpMethod = request.getHttpMethod();
+                if (httpMethod == "GET") {
+                    response = reqOauth.get(request.getRequestURL(), request.getParams());
+                } else if (httpMethod == "POST") {
+                    response = reqOauth.post(request.getRequestURL(), request.getParams());
+                } else if (httpMethod == "DELECT") {
+                    response = reqOauth.delete(request.getRequestURL(), request.getParams());
+                }
+            }
+            return response;
+        }
+
+        protected void onPostExecute(String response) {
+            controller(request, response);
+        }
+    }
+
+    private void runWorker(Request request) {
+        new Worker().execute(request);
     }
 
     /**
      * Get user request and put into the queue / Handler
+     *
      * @param requestUrl
      * @param params
      * @param httpMethod
@@ -72,23 +112,21 @@ public class RequestController {
     void request(String requestUrl, ArrayList<NameValuePair> params, String httpMethod, Handler handler) {
         Request request = new Request(requestUrl, params, httpMethod, handler);
         requestQueue.offer(request);
-        if(!worker.isAlive()){
-            runWorker();
-        }
+        controller();
     }
+
     /**
      * Get user request and put into the queue / RequestCallback
+     *
      * @param requestUrl
      * @param params
      * @param httpMethod
-     * @param handler
+     * @param callback
      */
     void request(String requestUrl, ArrayList<NameValuePair> params, String httpMethod, RequestCallback callback) {
         Request request = new Request(requestUrl, params, httpMethod, callback);
         requestQueue.offer(request);
-        if(!worker.isAlive()){
-            worker.start();
-        }
+        controller();
     }
 }
 
