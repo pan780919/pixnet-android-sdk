1e1e78b60c8895c0047e9ffc79f35352a2962dae
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-07-25 11:47:20 +0800

    Finish Comment.java,Reply.java , not test yet

--- pixnet-sdk/src/main/java/net/pixnet/sdk/response/Reply.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/response/Reply.java
@@ -1,6 +1,10 @@
 package net.pixnet.sdk.response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Reply{
+    Reply(String response){formatJson(response);}
     /**
      * Reply body
      */
@@ -9,4 +13,17 @@ public class Reply{
      * time reply created
      */
     public String created_at;
+    public void formatJson(String response){
+        try {
+            JSONObject obj = new JSONObject(response);
+            if(obj.has("body")){
+                body = obj.getString("body");
+            }
+            if(obj.has("created_at")){
+                created_at = obj.getString("created_at");
+            }
+        }catch (JSONException e){
+
+        }
+    }
 }
