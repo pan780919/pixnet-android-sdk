2db5e5fb72d4bb77ef0ead1a2843091d5ff8b551
Twbadkid <kkkoooiii2@yahoo.com.tw>
2014-08-08 14:59:21 +0800

    Modify Block's functions with DataProxy

--- pixnet-sdk/src/main/java/net/pixnet/sdk/utils/Block.java
+++ pixnet-sdk/src/main/java/net/pixnet/sdk/utils/Block.java
@@ -1,73 +1,59 @@
 package net.pixnet.sdk.utils;
 
-import android.content.Context;
 
-import net.pixnet.sdk.PIXNET;
-import net.pixnet.sdk.R;
+import android.text.TextUtils;
+
+import net.pixnet.sdk.proxy.DataProxy;
+import net.pixnet.sdk.response.BasicResponse;
+import net.pixnet.sdk.response.BlocksList;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
 import java.util.ArrayList;
 
-/**
- * Created by Koi on 2014/7/31.
- */
-public class Block {
-    private RequestController rc;
+public class Block extends DataProxy {
+    private static final String URL_BLOCK = "http://emma.pixnet.cc/blocks";
 
-    public Block(Context c) {
-        rc = RequestController.getInstance();
-        OAuthConnectionTool.OAuthVersion ver= PIXNET.getOAuthVersion(c);
-        OAuthConnectionTool tool;
-        if(ver== OAuthConnectionTool.OAuthVersion.VER_1){
-            tool=OAuthConnectionTool.newOaut1ConnectionTool(c.getString(R.string.consumer_key), c.getString(R.string.consumer_secret));
-            tool.setAccessTokenAndSecret(PIXNET.getOauthAccessToken(c), PIXNET.getOauthAccessSecret(c));
-        }
-        else{
-            tool=OAuthConnectionTool.newOauth2ConnectionTool();
-            tool.setAccessToken(PIXNET.getOauthAccessToken(c));
-        }
-        rc.setHttpConnectionTool(tool);
+    public void getBlockList() {
+        performAPIRequest(true, URL_BLOCK, new Request.RequestCallback() {
+            @Override
+            public void onResponse(String response) {
+                BlocksList res = new BlocksList(response);
+                listener.onDataResponse(res);
+            }
+        });
     }
-    public void getBlockList(String format,Request.RequestCallback callback){
-        ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-        if (format != null) {
-            params.add(new BasicNameValuePair("format", format));
+
+    public void addBlock(String user) {
+        if (user == null || TextUtils.isEmpty(user)) {
+            listener.onError(net.pixnet.sdk.proxy.Error.MISS_PARAMETER + ":user");
+            return;
         }
-
-        Request request = new Request("https://emma.pixnet.cc/blocks");
-        request.setParams(params);
-        request.setCallback(callback);
-
-        rc.addRequest(request);
+        ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("user", user));
+        performAPIRequest(true, URL_BLOCK + "/create", Request.Method.POST, new Request.RequestCallback() {
+            @Override
+            public void onResponse(String response) {
+                BasicResponse res = new BasicResponse(response);
+                listener.onDataResponse(res);
+            }
+        }, params);
     }
-    public void addBlock(String user,String format,Request.RequestCallback callback){
-        ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("user",user));
-        if (format != null) {
-            params.add(new BasicNameValuePair("format", format));
+
+    public void removeBlock(String user) {
+        if (user == null || TextUtils.isEmpty(user)) {
+            listener.onError(net.pixnet.sdk.proxy.Error.MISS_PARAMETER + ":user");
+            return;
         }
-
-        Request request = new Request("https://emma.pixnet.cc/blocks/create");
-        request.setMethod(Request.Method.POST);
-        request.setParams(params);
-        request.setCallback(callback);
-
-        rc.addRequest(request);
-    }
-    public void removeBlock(String user,String format,Request.RequestCallback callback){
         ArrayList<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new BasicNameValuePair("user",user));
-        if (format != null) {
-            params.add(new BasicNameValuePair("format", format));
-        }
-
-        Request request = new Request("https://emma.pixnet.cc/blocks/delete");
-        request.setMethod(Request.Method.DELETE);
-        request.setParams(params);
-        request.setCallback(callback);
-
-        rc.addRequest(request);
+        params.add(new BasicNameValuePair("user", user));
+        performAPIRequest(true, URL_BLOCK + "/delete", Request.Method.DELETE, new Request.RequestCallback() {
+            @Override
+            public void onResponse(String response) {
+                BasicResponse res = new BasicResponse(response);
+                listener.onDataResponse(res);
+            }
+        }, params);
     }
 }
